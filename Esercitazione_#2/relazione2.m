d4915c84a6c2500475963301571d940d92c65a6e
Giulio Coa <giulioc008@gmail.com>
2019-12-05 22:46:37 +0100

    Add files via upload

--- Esercitazione_#2/relazione2.m
+++ Esercitazione_#2/relazione2.m
@@ -31,10 +31,11 @@ grid on
 
 figure()
 x = [100, 5000, 8000, 10000, 15000, 16000, 17000, 18000, 20000, 25000, 29000, 1000000]; % vettore con le frequenze
-h_1 = R^2*(C + C_cavo);
-h_2 = 2*R*C*C_cavo;
-h_3 = R^2*C_cavo^2*C;
-H = tf([h_1 R], [h_3 h_2 C 0]); % funzione di trasferimento
+h_1 = R*C;
+h_2 = R^2*C*C_cavo;
+h_3 = R*(2*C_cavo + C);
+h_4 = R^2*C_cavo*(1 + C);
+H = tf([h_2 h_1 0], [h_4 h_3 1]); % funzione di trasferimento
 bode(H, 'b')
 hold on
 bode(H, x, 'r*')
