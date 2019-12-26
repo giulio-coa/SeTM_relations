84567266ad6928e2bf957f0885ae6432b1e68016
Giulio Coa <giulioc008@gmail.com>
2019-12-27 00:17:23 +0100

    Add files via upload

--- Esercitazione_#4/relazione4.m
+++ Esercitazione_#4/relazione4.m
@@ -2,6 +2,8 @@ clear all
 clear
 clc
 
+format long e
+
 % PREFISSI
 
 pico = 10^(-12);
@@ -25,11 +27,12 @@ R_u = 1 * k;
 % PUNTO 2.2.2
 
 x = [300, 1*k, 3*k, 10*k, 30*k, 100*k, 300*k, 1*M]; % vettore con le frequenze
-h_1 = A_v * (C_5 + C_10);
+h_1 = R_i * A_v * (C_5 + C_10);
 h_2 = R_u * C_6 + R_i * (C_5 + C_10);
 h_3 = R_i * R_u * C_6 * (C_5 + C_10);
 H = tf([h_1 0], [h_3 h_2 1]); % funzione di trasferimento
-magnitude = bode(H, x)
+magnitude = bode(H, x);
+magnitude = squeeze(magnitude)
 bode(H, 'b')
 hold on
 bode(H, x, 'r*')
