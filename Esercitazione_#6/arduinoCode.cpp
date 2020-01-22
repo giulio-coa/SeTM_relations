dd74fbf7465494ddc786086799573e5dc4dc2c2d
Giulio Coa <34110430+giulioc008@users.noreply.github.com>
2020-01-22 10:52:20 +0100

    Update arduinoCode.cpp

--- Esercitazione_#6/arduinoCode.cpp
+++ Esercitazione_#6/arduinoCode.cpp
@@ -1,18 +1,18 @@
 #include <string.h>
 
-//#define INT
+#define INT
 
-int D_out, N_B, pin, R_2, R_3, sleepTime;
-float S, T_C, T_K, V_out, V_FR;
+int D_out, N_B, pin, sleepTime;
+float R_2, R_3, S, T_C, T_K, V_out, V_FR;
 
 void setup() {
   Serial.begin(9600); // setup Serial
   pin = A0; // set the pin to read data
-  sleepTime = 2000;  // set how much time the sensor must execute the measurement of the temperature
+  sleepTime = 1000;  // set how much time the sensor must execute the measurement of the temperature
   S = 10 * pow(10, -3);
   N_B = 10;
-  R_2 = 1 * pow(10, 3);
-  R_3 = 3.8 * pow(10, 3);
+  R_2 = 3.8 * pow(10, 3);
+  R_3 = 1 * pow(10, 3);
 #ifdef INT
   V_FR = 1.1;
   analogReference(INTERNAL);  // set the type of alimentation
@@ -22,8 +22,8 @@ void setup() {
 }
 void loop() {
   D_out = analogRead(pin);  // read data from the pin
-  T_K = D_out * V_FR / pow(2, N_B) * 1 / S;
-  V_out = D_out * V_FR / pow(2, N_B);
+  T_K = (float) D_out * V_FR / pow(2, N_B) * 1 / S;
+  V_out = (float) D_out * V_FR / pow(2, N_B);
 #ifdef INT
   T_K = T_K * (1 + R_2 / R_3);
   V_out = V_out * (1 + R_2 / R_3);
